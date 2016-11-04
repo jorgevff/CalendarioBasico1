cf079d387f5e21ada7d7a8587d6d7b877624fe74
jorgevff <jvalladaresfontano@gmail.com>
2016-11-04 13:40:59 +0100

    se crea constructor sin parametros y se incializa las variables

---
+++ CalendarioBasico1.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class CalendarioBasico1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CalendarioBasico1
+{
+    // se crea atributo dia entero
+    private int dia;
+    //se crea atributo mes entero
+    private int mes;
+    //se crea atributo año entero
+    private int anno;
+
+    /**
+     * Constructor for objects of class CalendarioBasico1
+     */
+    public CalendarioBasico1()
+    {
+        // incializo las variables
+        dia = 1;
+        mes = 1;
+        anno = 1;
+    }
+
+    /**
+     * ahora voy a crear los metodos.
+     */
+    public void fecha()
+    {
+        // put your code here
+        
+    }
+}
